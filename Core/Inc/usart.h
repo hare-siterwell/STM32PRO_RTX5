0287d1135f046a692ed545285df8919835c52fff
Hare <renilr0@outlook.com>
2021-09-28 13:56:40 +0800

    .

--- Core/Inc/usart.h
+++ Core/Inc/usart.h
@@ -1,21 +1,21 @@
 /**
-  ******************************************************************************
-  * @file    usart.h
-  * @brief   This file contains all the function prototypes for
-  *          the usart.c file
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    usart.h
+ * @brief   This file contains all the function prototypes for
+ *          the usart.c file
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __USART_H__
 #define __USART_H__
@@ -38,8 +38,8 @@ extern UART_HandleTypeDef huart1;
 #define UART_REC_LEN 1024
 struct UartRx {
   osSemaphoreId_t sta;  // 串口接收标志
-  u8 buf[UART_REC_LEN]; // 串口接收缓冲
   u16 len;              // 串口接收长度
+  u8 buf[UART_REC_LEN]; // 串口接收缓冲
 };
 extern struct UartRx usart1;
 /* USER CODE END Private defines */
